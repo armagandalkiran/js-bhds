f5a64673632df5754bf7a077c16946b94cd25b66
Armağan Dalkıran <armagandalkiran@Armagan-MacBook-Pro.local>
2022-01-17 12:59:31 +0300

    first commit

---
+++ js-engine-and-runtime.js
@@ -0,0 +1,13 @@
+/*
+what is javascript engine actually ?
+
+Program that executes javascript is 
+javascript engine.
+
+every browser has its own js engine.
+without browser -> node js.
+
+
+
+
+*/ 
---
+++ notes-overview.js
@@ -0,0 +1,94 @@
+// DEFINITION
+
+// JavaScript is a high-level
+// object-oriented, multi-paradigm
+// programming language.
+
+// KEYWORDS 
+
+// high-level
+// prototype-based object-oriented
+// multi-paradigm
+// interpreted or just-in-time compiled
+// dynamic
+// single-threaded
+// garbage-collected
+// first-class functions
+// event loop concurrency model
+
+/*
+HİGH-LEVEL: 
+
+Any computer program needs resources
+
+Low level languages like C Developers
+has to manage resources manually
+
+High level languages like python Developers
+does not have to worry, everything happens
+automatically.
+
+GARBAGE-COLLECTED:
+
+Powerful tool of taking memory management
+away from developers.
+
+- an algorithm inside JavaScript engine.
+- removes old, unused objects from memory.
+
+INTERPRETED or JUST-IN-TIME COMPILED
+
+The intermediate code is converted into 
+machine language only when the application
+needs that is required 
+codes are only converted to machine code.
+
+
+MULTI-PARADIGM
+
+paradigm: An approach and mindset of structuring
+code, which will direct your coding style and technique.
+
+1) Procedural programming
+2) Object oriented programming
+3) Functional programming
+
+PROTOTYPE-BASED OBJECT-ORIENTED
+
+FİRST-CLASS FUNCTIONS
+
+In a language with first-class functions, functions
+are simply treated as variables. We can pass
+them into other functions, and return them
+from functions.
+
+
+DYNAMIC
+
+No data type definitions. Types becomes
+known at runtime.
+
+Data type of variable is automatically changed.
+
+
+SINGLE-THREADED
+
+concurrency model: How the JavaScript engine
+handles multiple tasks happening at the same time.
+
+why do we need that ?
+
+JavaScript runs in one single thread, so
+it can only do one thing at a time.
+
+so what about a long-running task ?
+
+Sounds like it would block the single thread.
+However, we want non-blocking behavior.
+
+how do we achieve that then ?
+
+By using an EVENT LOOP: takes long running
+tasks, executes them in the "background", and
+puts them back in the main thread once they are finished.
+*/ 
